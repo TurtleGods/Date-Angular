063ae70a6c0e0a213f8bfc05a966ebc2d80a1b8f
Turtlegods <strollist@gmail.com>
2025-05-13 22:51:13 +0800

    加入toast

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-nav',
   standalone: true,
@@ -13,6 +14,7 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 export class NavComponent {
   accountService = inject(AccountService);
   private router = inject(Router);
+  private taasttr = inject(ToastrService);
   model: any = {};
 
   login() {
@@ -20,7 +22,7 @@ export class NavComponent {
       next: _ => {
         this.router.navigateByUrl('/members');
       },
-      error: (error) => console.log(error),
+      error: (error) => this.taasttr,
     });
   }
   logout() {
