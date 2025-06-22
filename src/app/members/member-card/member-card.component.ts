ec74e5498b64ae810cc04ed3ea06791e28a96ec9
Turtlegods <strollist@gmail.com>
2025-06-23 00:09:38 +0800

    creating Member cards

---
+++ src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,13 @@
+import { Member } from './../../_models/member';
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-member-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-card.component.html',
+  styleUrl: './member-card.component.css'
+})
+export class MemberCardComponent {
+  member = input.required<Member>();
+}
